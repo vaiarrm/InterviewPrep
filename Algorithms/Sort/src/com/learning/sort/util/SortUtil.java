3f2ecb887e3d8846cab12083392cfe4fbb21a319
Vaibhav Sharma <vaibhav.s.sharma@outlook.com>
2019-04-14 19:23:03 -0400

    Initial Commit - SortUtil

---
+++ Algorithms/Sort/src/com/learning/sort/util/SortUtil.java
@@ -0,0 +1,19 @@
+package com.learning.sort.util;
+
+public class SortUtil {
+	
+	private SortUtil() {}
+	
+	public static <T>  void swap(T [] arr, int i, int j ) {
+		T temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	public static<T> void printArr(T [] arr) {
+		for (T elem : arr) {
+			System.out.println(elem.toString());
+		}
+	}
+
+}
