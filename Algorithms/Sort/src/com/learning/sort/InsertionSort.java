9ba624f5b203bdc10629df287de58c31d0c90b8b
Vaibhav Sharma <vaibhav.s.sharma@outlook.com>
2019-04-15 06:41:28 -0400

    adding main method

--- Algorithms/Sort/src/com/learning/sort/InsertionSort.java
+++ Algorithms/Sort/src/com/learning/sort/InsertionSort.java
@@ -5,15 +5,18 @@ import com.learning.sort.util.SortUtil;
 /**
  * @author vaibhavsharma
  * https://www.interviewbit.com/tutorial/insertion-sort-algorithm/
+ * 
+ * Logic is to keep all the elements to the left of element in the
+ * array smaller than element
  */
 public class InsertionSort{
-	
+
 	private InsertionSort() {}
-	
+
 	public static <T extends Comparable<T>>  void sort(T [] arr) {
-		
+
 		if (arr == null || arr.length ==1 ) return;
-		
+
 		for (int i = 1; i < arr.length; i++ ) {
 			int curr = i;
 			for (int j = i-1; j >= 0 ; j--) {
@@ -25,5 +28,14 @@ public class InsertionSort{
 			}
 		}
 	}
+
+	public static void main(String[] args) {
+
+		Integer [] arr = new Integer[] {10,9,8,7,6,5,4,3,2,1}; 
+		InsertionSort.sort(arr);
+		SortUtil.printArr(arr);
+
+	}
+
 
 }
--- Algorithms/Sort/src/com/learning/sort/SelectionSort.java
+++ Algorithms/Sort/src/com/learning/sort/SelectionSort.java
@@ -3,6 +3,8 @@
  */
 package com.learning.sort;
 
+import com.learning.sort.util.SortUtil;
+
 /**
  * @author vaibhavsharma
  *
@@ -11,6 +13,22 @@ public class SelectionSort {
 	
 	private SelectionSort() {}
 	
-	public static <T extends Comparable<T>>  void sort(T [] arr) {}
-
+	public static <T extends Comparable<T>>  void sort(T [] arr) {
+		
+		for (int i = 0; i < arr.length - 1 ; i ++) {
+			int min = i;
+			for (int j = i+1; j < arr.length ; j++) {
+				if (arr[j].compareTo(arr[min])<0) {
+					min = j;
+				}
+			}
+			SortUtil.swap(arr, min, i);
+		}
+	}
+	
+	public static void main(String ... args) {
+		Integer [] arr = new Integer[] {10,9,8,7,6,5,4,3,2,1}; 
+		SelectionSort.sort(arr);
+		SortUtil.printArr(arr);
+	}
 }
